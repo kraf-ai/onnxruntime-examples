4caae9dbef0193d8775f38d2597458aa43aac660
Aarón Espasandín <aaespasa@gmail.com>
2025-02-25 20:34:42 +0100

    Base code for the MNIST example

--- 01.MNIST/MNIST/MNIST.cpp
+++ 01.MNIST/MNIST/MNIST.cpp
@@ -1,20 +1,13 @@
-// MNIST.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "../MLCore/adapter.h"
+#include "../MLCore/utils.h"
 
-#include <iostream>
+int main() {
+    Logger::Init("log_mlcore.txt");
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    auto dmlExecutionProvider = OrtUtils::ExecutionProvider::DirectML;
+    std::cout << "Most powerful device for the execution provider " + OrtUtils::ToProviderString(dmlExecutionProvider) + ":" << std::endl;
+    MLCore::AdapterInfo powerfulAdapter = MLCore::GetMostPowerfulAdapter(dmlExecutionProvider);
+    MLCore::PrintAdapterDetails(powerfulAdapter);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
